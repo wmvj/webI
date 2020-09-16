b9813bf72de167142f7ab193546c028e802241ab
Warley Junior <warleyjunior05@gmail.com>
2020-09-16 14:30:44 -0300

    Sexto exemplo

--- html/form/server.js
+++ html/form/server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/usuarios',(req, resp) => {
+app.post('/usuarios', (req, resp) => {
     console.log(req.body)
     console.log(req.query)
     resp.send('<h1> Parabéns. Usuário incluído!! </h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1> Parabéns. Usuário alterado!! </h1>')
 })
 
 app.listen(3003)
