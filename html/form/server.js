6d4580be7331a110474850b7552722324e7cf347
Warley Junior <warleyjunior05@gmail.com>
2020-10-07 12:19:48 -0300

    Sétimo Exemplo

--- html/form/server.js
+++ html/form/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
     console.log(req.query)
-    resp.send('<h1> Parabéns. Usuário incluído!! </h1>')
+    resp.send(`<h1> Parabéns. Usuário incluído!! </h1>`)
 })
 
 app.post('/usuarios/:id', (req, resp) => {
