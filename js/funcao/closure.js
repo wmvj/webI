6f4a0b4c543e8412eb4b34ebefccaae20b363600
Warley Junior <warleyjunior05@gmail.com>
2020-12-03 01:56:09 -0300

    AULA 13 - Funções & Objetos

---
+++ js/funcao/closure.js
@@ -0,0 +1,12 @@
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao)
